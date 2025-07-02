7c5f902c50f83fdf1d587c9aaa1e7accde99d7b0
Daria Greb <kibergucci@bk.ru>
2025-07-03 02:59:49 +0700

    Создание файла main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+import cv2
+import numpy as np
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton,
+                             QLabel, QFileDialog, QLineEdit, QHBoxLayout, QMessageBox, QComboBox)
+from PyQt5.QtGui import QPixmap, QImage, QIntValidator
+from PyQt5.QtCore import Qt
